ef9c18e65226b8b3724716b018bdc7ecae2e1583
Cliff Sun <cliffsun91@gmail.com>
2014-11-01 23:30:19 +0000

    refactored the temp reader classes

--- temp_monitor.sh
+++ temp_monitor.sh
@@ -2,12 +2,12 @@
 
 case "$1" in
   start)
-    python /home/pi/projects/RPi_sousvide/temp_reader.py & > /dev/null
+    python /home/pi/projects/RPi_sousvide/loop_temp_reader.py & > /dev/null
 	python /home/pi/projects/RPi_sousvide/sousvideserver.py & > /dev/null 
     ;;
   stop)
 	sudo kill -9 `pgrep -f 'python .*sousvideserver.py'`
-    sudo kill -TERM `pgrep -f 'python .*temp_reader.py'`
+    sudo kill -TERM `pgrep -f 'python .*loop_temp_reader.py'`
     ;;
   *)
     echo "Usage: /temp_monitor.sh {start|stop}"
--- temp_reader.py
+++ temp_reader.py
@@ -8,63 +8,6 @@ import sys
 #os.system('modprobe w1-therm')
 
 
-class LoopTempReader(object):
-	def __init__(self, log_file="/home/pi/projects/RPi_sousvide/temps.log", max_lines=40):
-		self.log_file = log_file
-		self.max_lines = max_lines
-		self.file_writer = FileWriter(self.log_file)
-
-	def loop_read(self):
-		sensor_repo = SensorRepository()
-		sensors = sensor_repo.get_all_sensors()
-		if len(sensors) == 0:
-			raise Exception("No sensors found! Check '/sys/bus/w1/devices/' for sensor devices, you may have to run sudo modprobe w1-gpio/therm")
-		reader = TempReader(sensors)
-		self.truncate_temp_logs(self.log_file, self.max_lines)
-		self.file_writer.append_to_end_of_file("---------- Temp Logging Started {0} ----------\n".format(self.get_time_now()))
-		while True:
-			try:
-				sleep(10)
-				temps = reader.read_temps()
-				time = self.get_time_now()
-				index = 0
-				for temp in temps:
-					index += 1
-					temp_string = u"Sensor {0} - time: {1} | temp: {2}{3}\n".\
-										format(index, time, str(temp), u"\u00b0"+'C')
-					#print temp_string.encode("UTF-8")
-					self.file_writer.append_to_end_of_file(temp_string)
-				self.truncate_temp_logs(self.log_file, self.max_lines)
-			except (KeyboardInterrupt, SystemExit):
-				print 'Stopping writing to temp logs now! Adding end marker to temp file'
-				self.file_writer.append_to_end_of_file("---------- Temp Logging Stopped ----------\n")
-				raise
-
-	def truncate_temp_logs(self, log_file, max_lines):
-		num_lines = sum(1 for line in open(log_file))
-		if num_lines > max_lines:
-			with open(log_file, 'r') as f_read:
-				lines = f_read.readlines()
-			with open(log_file, 'w') as f_overwrite:
-				f_overwrite.writelines(lines[num_lines-max_lines:])
-
-	def get_time_now(self):
-		time_now = datetime.datetime.now()
-		time_first = time_now.strftime("%H:%M:%S")
-		time_millis = time_now.strftime(".%f")
-		time_millis_rounded = round(float(time_millis), 2)
-		return "{0}{1}".format(time_first, "{0:.2f}".format(time_millis_rounded)[1:])
-		
-
-class FileWriter(object):
-	def __init__(self, file_path):
-		self.file_path = file_path
-
-	def append_to_end_of_file(self, text):
-		with open(self.file_path, "a+") as f:
-			f.write(text.encode('utf8'))
-
-
 class SensorRepository(object):
 	def __init__(self, base_dir='/sys/bus/w1/devices/'):
 		self.base_dir = base_dir
@@ -112,10 +55,3 @@ class TempSensor(object):
 			else:
 				raise Exception('could not read temp \'t=...\' from raw_data: {0}'.format(str(raw_data)))
 
-
-def main():
-	loopReader = LoopTempReader()
-	loopReader.loop_read()
-
-if __name__ == "__main__":
-	main()
